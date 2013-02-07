17f283e84e1226e7eee1300bdc513fbc5cdf74fc
Lars Doucet <lars.doucet+github@gmail.com>
2013-02-07 14:39:03 -0600

    Initial commit - hope I did it right!

---
+++ source/Main.hx
@@ -0,0 +1,18 @@
+import nme.Lib;
+import org.flixel.FlxG;
+import org.flixel.FlxGame;
+import org.flixel.plugin.leveluplabs.example.State_Title;
+	
+class Main extends FlxGame
+{	
+	public function new()
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+		var ratioX:Float = stageWidth / 800;
+		var ratioY:Float = stageHeight / 600;
+		var ratio:Float = Math.min(ratioX, ratioY);
+		super(Math.floor(stageWidth / ratio), Math.floor(stageHeight / ratio), State_Title, ratio, 30, 30);
+		forceDebugger = true;		
+	}
+}
