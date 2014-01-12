ef9f3551e666b5a89947951ddc3d1761c9a7e838
Gama11 <jensfischer95@gmail.com>
2014-01-12 17:55:41 +0100

    FlxUIText cleanup FlxText.textField has recently been made public, so some of these functions are obsolete

--- flixel/addons/ui/FlxUIText.hx
+++ flixel/addons/ui/FlxUIText.hx
@@ -12,19 +12,11 @@ class FlxUIText extends FlxText implements IResizable implements IFlxUIWidget
 {
 	public var id:String; 
 	
-	public function new(X:Float, Y:Float, Width:Int, Text:String = null, size:Int=8, EmbeddedFont:Bool = true)
-	{
-		super(X, Y, Width, Text, size, EmbeddedFont);
-	}
-	
 	public function resize(w:Float, h:Float):Void {
 		width = w;
 		height = h;
 		calcFrame();
 	}
-	
-	public function textWidth():Float {	return _textField.textWidth; }
-	public function textHeight():Float { return _textField.textHeight; }
 	
 	public function forceCalcFrame():Void {
 		#if flash
@@ -32,10 +24,5 @@ class FlxUIText extends FlxText implements IResizable implements IFlxUIWidget
 		#else
 			calcFrame(true);
 		#end
-	}
-	
-	public function getTextField()
-	{
-		return _textField;
 	}
 }
