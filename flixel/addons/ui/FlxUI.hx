e05d97418339107170fdc7d9d31a056989f10636
Richard Wardin <shalmezad@gmail.com>
2014-07-15 12:57:37 -0400

    Moved input text to use input_text (instead of text with input=1)

--- flixel/addons/ui/FlxUI.hx
+++ flixel/addons/ui/FlxUI.hx
@@ -992,6 +992,7 @@ class FlxUI extends FlxUIGroup implements IEventGetter
 			case "line": return _loadLine(info);
 			case "sprite": return _loadSprite(info);
 			case "text": return _loadText(info);								//if input has events
+			case "input_text": return _loadInputText(info);								//if input has events
 			case "numeric_stepper": return _loadNumericStepper(info);			//has events, params
 			case "button": return _loadButton(info);							//has events, params
 			case "button_toggle": return _loadButton(info,true,true);			//has events, params
@@ -1517,6 +1518,65 @@ class FlxUI extends FlxUIGroup implements IEventGetter
 		
 		return ft;
 	}
+
+	private function _loadInputText(data:Fast):IFlxUIWidget{
+		
+		var text:String = U.xml_str(data.x, "text");
+		var context:String = U.xml_str(data.x, "context", true, "ui");
+		var code:String = U.xml_str(data.x, "code", true, "");
+		text = getText(text,context, true, code);
+		
+		var W:Int = Std.int(_loadWidth(data, 100));
+		
+		var the_font:String = _loadFontFace(data);
+		
+		var align:String = U.xml_str(data.x, "align"); if (align == "") { align = null;}
+		var size:Int = U.xml_i(data.x, "size"); if (size == 0) { size = 8;}
+		var color:Int = _loadColor(data);
+		
+		var border:BorderDef = _loadBorder(data);
+		
+		var backgroundColor:Int = U.parseHex(U.xml_str(data.x, "background", true, "0x00000000"), true, true, 0x00000000);
+		
+		var ft:IFlxUIWidget;
+		var fti:FlxUIInputText = new FlxUIInputText(0, 0, W, text, size, color, backgroundColor);
+			
+		var force_case:String = U.xml_str(data.x, "force_case", true, "");
+		var forceCase:Int;
+		switch(force_case) {
+			case "u", "upper", "upper_case", "uppercase": forceCase = FlxInputText.UPPER_CASE;
+			case "l", "lower", "lower_case", "lowercase": forceCase = FlxInputText.LOWER_CASE;
+			default: forceCase = FlxInputText.ALL_CASES;
+		}
+		
+		var filter:String = U.xml_str(data.x, "filter", true, "");
+		var filterMode:Int;
+		while (filter.indexOf("_") != -1) {
+			filter = StringTools.replace(filter, "_", "");	//strip out any underscores
+		}
+		switch(filter) {
+			case "a", "alpha", "onlyalpha": filterMode = FlxInputText.ONLY_ALPHA;
+			case "n", "num", "numeric", "onlynumeric": filterMode = FlxInputText.ONLY_NUMERIC;
+			case "an", "alphanum", "alphanumeric", "onlyalphanumeric": filterMode = FlxInputText.ONLY_ALPHANUMERIC;
+			default: filterMode = FlxInputText.NO_FILTER;
+		}
+			
+		fti.setFormat(the_font, size, color, align);
+		fti.forceCase = forceCase;
+		fti.filterMode = filterMode;
+		border.apply(fti);
+		fti.drawFrame();
+		ft = fti;
+		
+		if (data.hasNode.param) {
+			var params = getParams(data);
+			var ihp:IHasParams = cast ft;
+			ihp.params = params;
+		}
+		
+		return ft;
+	}
+
 	
 	public static function consolidateData(data:Fast, definition:Fast):Fast {
 		if (data == null && definition != null) {
